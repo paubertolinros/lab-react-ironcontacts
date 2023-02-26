ef33d357da6ae121ed20dcb4a612b006bc812e30
Pau Bertolin <paulaberos@gmail.com>
2023-02-26 18:52:08 +0100

    try another functions

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ function App() {
   const [contacts, setContacts] = useState(contactList.slice(0,5));
   const handleAddRandomContact = () => {
     const randomContact = contactList[Math.floor(Math.random() * contactList.length)]
+    let newListContacts= [...contacts]
     if (!contacts.includes(randomContact)) {
-      setContacts([...contacts, randomContact])
-      //QUESTION: perquè no funciona si poso contacts sense l'spread?
+      //const newContacts = [...contacts, randomContact];
+      newListContacts.push(randomContact)
+      setContacts(newListContacts);
+      //QUESTION: perquè no funciona si poso contacts sense l'spread? (línia 11 o 13)
+      // és perquè només setContacts pot modificar lestat contacts? és a dir, tot i que no
+      //utlitzi un mètode que modifiqui l'array original, si la modifico jo amb un
+      //push o amb una col·lecció indexada, haig de fer servir l'spread per fer una còpia? 
+      //És que no sé si ho acabo d'entendre...
       //Perquè em quedi clar, ho passo tot dins de [ ] perquè sinó
-      //no podria fer el map, no?
+      //no podria fer el map, no? (línia 13)
     }
   };
+  console.log("contaxts",contacts)
+  console.log("...contactc", [...contacts])
 
   const handleSortByName = () => {
     const sortByName = [...contacts].sort((a, b) => a.name.localeCompare(b.name));
-    console.log(sortByName)
     setContacts(sortByName)
   }
 
@@ -27,11 +35,18 @@ function App() {
     setContacts(sortByPopularity)
   }
 
+  // const handleDeleteContact = (contactId) => {
+  //   const filterContacts = contacts.filter(elem => {
+  //     return elem.id !== contactId
+  //   });
+  //   setContacts(filterContacts)
+  // }
+
   const handleDeleteContact = (contactId) => {
-    const filterContacts = contacts.filter(elem => {
-      return elem.id !== contactId
-    });
-    setContacts(filterContacts)
+    let newContactsList = [...contacts]
+    const filterContacts = newContactsList.findIndex(elem => elem.id === contactId);
+    newContactsList.splice(filterContacts, 1); 
+    setContacts(newContactsList)
   }
 
   return (
